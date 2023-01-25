7ddf41581dae09332d29a638a61f4efd851f2f15
rhenjq1112 <122388092+rhenjq1112@users.noreply.github.com>
2023-01-26 03:02:46 +0500

    В ViewController был добавлен код для работы программы по считыванию нажатий на кнопку.

--- Spint3Shmakov/ViewController.swift
+++ Spint3Shmakov/ViewController.swift
@@ -8,7 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count=0
+    @IBOutlet weak var theLabel: UILabel!
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        count+=1
+        theLabel.text="Значение счетчика:\(count)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
